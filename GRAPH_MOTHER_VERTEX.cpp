117a2d465a97d654fee8cf8546be1ffd2a05f1a1
sagarsingla14 <43108535+sagarsingla14@users.noreply.github.com>
2019-03-30 17:44:38 +0530

    Update GRAPH_MOTHER_VERTEX.cpp

--- GRAPH_MOTHER_VERTEX.cpp
+++ GRAPH_MOTHER_VERTEX.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 #define ll long long
 using namespace std;
-ll t;
 vector <ll> graph[1000];
+ll n,e,temp;
+
 int insert(ll u,ll v){
 	graph[u].push_back(v);
 }
-vector <ll> visited(10,0);  ////////////// problem is heeree
+
+vector <ll> visited(1000,0);
 void dfs(ll cur){
 	visited[cur]=1;
 	for(ll z=0;z<graph[cur].size();z++){
@@ -16,22 +18,24 @@ void dfs(ll cur){
 		}
 	}
 }
+
 ll mothervertex=0;
 void findMotherVertex(){
-	for(ll i=0;i<t;i++){
+	for(ll i=0;i<n;i++){
 		if(visited[i]!=1){
 			dfs(i);
 			mothervertex=i;
 		}
 	}
 }
+
 int CMV(){
 	ll flag=0;
-	for(ll i=0;i<t;i++){
+	for(ll i=0;i<n;i++){
 		visited[i]=0;
 	}
 	dfs(mothervertex);
-	for(ll i=0;i<t;i++){
+	for(ll i=0;i<n;i++){
 		if(visited[i]!=1){
 			flag=1;
 			break;
@@ -45,13 +49,14 @@ int CMV(){
 	}
 }
 int main(){
-	ll n;
-	cin>>t>>n;	
-	while(n--){
+	cin>>n>>e;	 
+	temp=e;
+	while(temp--){
 		ll u,v;
 		cin>>u>>v;
 		insert(u,v);
 	}
+	temp=n;
 	findMotherVertex();
 	ll vertex=CMV();
 	if(vertex==-1){
